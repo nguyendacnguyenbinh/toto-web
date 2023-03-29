fe1e266eabcf2fbfaab7accb36a20212e1d27648
Nguyen Dac Nguyen Binh <nguyendacnguyenbinh@gmail.com>
2023-03-29 16:05:39 +0700

    add show onscroll

--- asset/js/javascript.js
+++ asset/js/javascript.js
@@ -1,7 +1,7 @@
 "use strict";
 
 var hasSubMenu = document.querySelector('.has-sub-menu');
-var piece = document.querySelectorAll('.onePiece');
+var pieces = document.querySelectorAll('.onePiece');
 var slides = document.querySelectorAll('.main-container__slide--links');
 var slide = 2;
 var btnBTT = document.querySelector('.btn-back-to-top');
@@ -15,10 +15,7 @@ var modal = document.querySelector('.modal');
 
 (function start() {
     addBlackFriday ();    
-    // listen for events
-    window.addEventListener("load", callbackFunc);
-    window.addEventListener("resize", callbackFunc);
-    window.addEventListener("scroll", callbackFunc);
+
     showSlide();
 })()
 
@@ -30,6 +27,22 @@ function addBlackFriday () {
   })  
 }
 
+function showSlide() { 
+    
+  if(slide === (slides.length - 1 )){
+    slides[slide].classList.remove('show'); 
+    slides[0].classList.add('show');        
+    slide = 0;     
+  } else {
+    slides[slide].classList.remove('show');
+    slides[slide + 1].classList.add('show'); 
+    slide++;  
+  }
+setTimeout(showSlide, 3000)    
+}
+
+
+
 const menu = document.querySelector('#menu');
 const sideBar = document.querySelector('.sidebar');
 
@@ -37,6 +50,7 @@ menu.onclick = function() {
   
 }
 
+// Đóng modal cover
 var closes = document.querySelectorAll('.close-modal');
 for (const close of closes) {
   close.addEventListener('click', () => {
@@ -44,78 +58,23 @@ for (const close of closes) {
   })
 }
 
-// check if an element is in viewport
-  // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
-  function isElementInViewport(el) {
-    var rect = el.getBoundingClientRect();
-    return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <=
-        (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-  }
+// show onscroll
+function checkAnimationScroll() {
+  pieces.forEach(function (piece) {
+    let heightScreen = window.innerHeight;
+    let rectEl = piece.getClientRects()[0];
 
-  
-  function callbackFunc() {
-    for (var i = 0; i < piece.length; i++) {
-      if (isElementInViewport(piece[i])) {
-        piece[i].classList.add("in-view");
-      }
-    }
-  }
+    console.log(rectEl);
 
-
-
-
-// function scroll (){
-//   let point = 0;
-//   document.onscroll = function() {
-//     point = window.scrollY || document.documentElement.scrollTop;
-//     // console.log(point)
-//     if (point < 100){
-//       for (var i = 0; i < piece.length ; i++){
-//         piece[i].classList.remove('in-view')    
-//       }
-    
-//     } else if (point >= 100 && point < 800) {
-//       piece[0].classList.add('in-view'); 
-//     } else if (point >= 800 && point < 1500) {
-//       piece[1].classList.add('in-view');
-   
-//     } else if (point >= 1500 && point < 2800) {
-//       piece[2].classList.add('in-view');  
-      
-//     } else {
-//       piece[3].classList.add('in-view');    
-//     }
-
-//     if( point < 150) {
-//       btnBTT.classList.remove('display');
-//     } else {
-//       btnBTT.classList.add('display');
-//     }
-//   }  
-  // logic sai. 
-  // Khi load lại ở giữa trang.
-  //  Các mục sẽ ko hiện ra mà phải kéo từ trên xuống mới hiện 
-// }
-
-
-function showSlide() { 
-    
-    if(slide === (slides.length - 1 )){
-      slides[slide].classList.remove('show'); 
-      slides[0].classList.add('show');        
-      slide = 0;     
+    if(!(rectEl.top > heightScreen || rectEl.bottom < 0)){
+      piece.classList.add("in-view");
     } else {
-      slides[slide].classList.remove('show');
-      slides[slide + 1].classList.add('show'); 
-      slide++;  
+      piece.classList.remove("in-view");
     }
-  setTimeout(showSlide, 3000)    
+  })
 }
+
+window.onscroll = checkAnimationScroll
 
 
 
